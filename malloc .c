17c57d0b50b429f3eb3ee3e2d6b0476c6f09b539
Raushan Kumar <123895684+Raushan1845@users.noreply.github.com>
2026-06-14 23:04:43 +0530

    Create malloc .c

---
+++ malloc .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr;
+
+    // Allocate memory for one integer
+    ptr = (int *)malloc(sizeof(int));
+
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    *ptr = 100;
+
+    printf("Value = %d\n", *ptr);
+
+    free(ptr);  // Free allocated memory
+
+    return 0;
+}
